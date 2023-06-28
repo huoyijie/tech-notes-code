2aa7c6c696257d490af0bee578debb808ad211e1
huoyijie <yijie.huo@foxmail.com>
2023-06-28 15:20:40 +0800

    refactor code

--- oauth2-mysql-store/oauth2/server.go
+++ oauth2-mysql-store/oauth2/server.go
@@ -17,6 +17,7 @@ type AuthServer struct {
 	s *server.Server
 }
 
+// 创建 OAuth2 server
 func NewAuthServer(tokenStore oa2.TokenStore, clientStore oa2.ClientStore, r *gin.Engine) *AuthServer {
 	manager := manage.NewDefaultManager()
 	manager.MapTokenStorage(tokenStore)
@@ -24,11 +25,13 @@ func NewAuthServer(tokenStore oa2.TokenStore, clientStore oa2.ClientStore, r *gi
 
 	s := server.NewDefaultServer(manager)
 
+	// token endpoint
 	g := r.Group("oauth")
 	{
 		g.POST("token", func(c *gin.Context) {
 			err := s.HandleTokenRequest(c.Writer, c.Request)
 			if err != nil {
+				// todo: 真实应用中，需返回具体错误码给客户端
 				c.AbortWithError(http.StatusBadRequest, err)
 				return
 			}
@@ -38,10 +41,12 @@ func NewAuthServer(tokenStore oa2.TokenStore, clientStore oa2.ClientStore, r *gi
 	return &AuthServer{s}
 }
 
+// Token 有效验证拦截器
 func (as *AuthServer) HandleTokenVerify() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		ti, err := as.s.ValidationBearerToken(c.Request)
 		if err != nil {
+			// todo: 真实应用中，需返回具体错误码给客户端，客户端根据错误码决定下一步动作(如: access_token 过期则刷新 token, refresh_token 过期则重新登录等)
 			c.AbortWithError(http.StatusUnauthorized, err)
 			return
 		}
@@ -50,6 +55,7 @@ func (as *AuthServer) HandleTokenVerify() gin.HandlerFunc {
 	}
 }
 
+// OAuth Password Grant Handler
 func (as *AuthServer) SetPasswordAuthorizationHandler(handler server.PasswordAuthorizationHandler) {
 	as.s.PasswordAuthorizationHandler = handler
 }
