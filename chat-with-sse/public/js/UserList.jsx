c82f4f5483b321b67ba617c5f575cb9d994c0a75
huoyijie <yijie.huo@foxmail.com>
2023-07-13 16:17:47 +0800

    refactor userlist avatar align

--- chat-with-sse/public/js/UserList.jsx
+++ chat-with-sse/public/js/UserList.jsx
@@ -1,9 +1,11 @@
 function UserList() {
   return (
     <div className="col-span-1 border rounded-s flex flex-col divide-y">
-      <div className="basis-1/12 py-4 px-5 bg-sky-50 text-center">
-        <img src={`public/images/${User.Username}.svg`} className="inline-block h-12 w-12 rounded-full ring-2 ring-white" />
-        <h1>{User.Username}</h1>
+      <div className="basis-1/12 py-4 bg-sky-50">
+        <div className="w-12 mx-auto">
+          <img src={`public/images/${User.Username}.svg`} className="inline-block h-12 w-12 rounded-full ring-2 ring-white" />
+          <h1>{User.Username}</h1>
+        </div>
       </div>
       <Users />
     </div>
