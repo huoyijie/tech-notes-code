e0cafd2d8ae49e08a6a2d102a8c5a47761f76774
huoyijie <yijie.huo@foxmail.com>
2023-07-13 15:16:58 +0800

    refactor userlist add avatar

--- chat-with-sse/public/js/UserList.jsx
+++ chat-with-sse/public/js/UserList.jsx
@@ -1,8 +1,9 @@
 function UserList() {
   return (
     <div className="col-span-1 border rounded-s flex flex-col divide-y">
-      <div className="basis-1/12 py-4 px-2 bg-sky-50">
-        <h1 className="text-2xl text-amber-600">{User.Username}</h1>
+      <div className="basis-1/12 py-4 px-5 bg-sky-50 text-center">
+        <img src={`public/images/${User.Username}.svg`} className="inline-block h-12 w-12 rounded-full ring-2 ring-white" />
+        <h1>{User.Username}</h1>
       </div>
       <Users />
     </div>
