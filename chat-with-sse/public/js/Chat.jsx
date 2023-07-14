59a262a6c5df96f29122af1a88efb690cea08646
huoyijie <yijie.huo@foxmail.com>
2023-07-14 10:38:21 +0800

    调整 chat md 宽度

--- chat-with-sse/public/js/Chat.jsx
+++ chat-with-sse/public/js/Chat.jsx
@@ -1,6 +1,6 @@
 function Chat() {
   return (
-    <div className="w-full md:w-4/5 h-4/5 mx-auto mt-5 grid grid-cols-4 md:grid-cols-8 bg-white">
+    <div className="w-full md:w-3/5 h-4/5 mx-auto mt-5 grid grid-cols-4 md:grid-cols-8 bg-white">
       <UserList />
       <ChatBox />
     </div>
